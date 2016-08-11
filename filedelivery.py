e729b51caa80a65414e59bbc2a2f0fee5f40cb29
Thomas Judd <tjudd@elitedaily.com>
2016-08-11 18:52:16 -0400

    removes networkx, partial dijkstra implementation

--- filedelivery.py
+++ filedelivery.py
@@ -1,14 +1,11 @@
 #!flask/bin/python
 
 from flask import Flask, request, jsonify
-import networkx as nx
+import math
 
 app = Flask(__name__)
 
-myhosts = []
-graph = nx.Graph()
-
-mylinks = {}
+graph = {}
 
 @app.route('/')
 def index():
@@ -18,10 +15,13 @@ def index():
 def host():
   data = request.get_json()
   if request.method=='POST':
-    graph.add_node(data['name'] , data)
-    return jsonify(graph.nodes())
-  else:
-    return jsonify({})
+     graph[data['name']] = {
+       'name': data['name'],
+       'cost': 999999, #sufficiently large inital value gets overwritten in dijkstra
+       'visited': False,
+       'neighbor_keys': []
+     }
+  return jsonify(graph)
 
 @app.route('/hosts', methods=['GET'])
 def hosts():
@@ -30,20 +30,38 @@ def hosts():
 @app.route('/link', methods=['POST'])
 def link():
   data = request.get_json()
-  if len(myhosts) < 0:
-    return None
 
-  graph.add_edge(data['source_host'], data['dest_host'])
+  graph[data['source_host']]['neighbor_keys'].append(graph[data['dest_host']]['name'])
+  graph[data['dest_host']]['neighbor_keys'].append(graph[data['source_host']]['name'])
 
-  return jsonify(graph.edges())
+  return jsonify(graph)
 
 @app.route('/links', methods=['GET'])
 def links():
-  return jsonify(graph.edges())
+  return jsonify(graph)
 
 @app.route('/path/<host_a>/to/<host_b>', methods=['GET'])
 def find_cheapest_path(host_a, host_b):
-  return jsonify(nx.shortest_path(graph, host_a, host_b))
+  start_node['cost'] = 0
+  EDGE_COST = 1
+
+  curr_node = start_node
+  
+  while len(visted) < len(nodes):
+    curr_node['visited'] = True
+    visited.append(curr_node)
+    for node in curr_node['neighbors']:
+      min_node = None
+      if node['cost'] > curr_node['cost'] + EDGE_COST and not node['visited']:
+        node['cost'] = curr_node['cost'] + EDGE_COST
+        if min_node['cost'] > node['cost']:
+          min_node = node 
+    curr_node = min_node
+
+  return jsonify(visited)
+
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
