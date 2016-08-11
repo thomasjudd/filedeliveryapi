6ab6dc09b02c261d360e3d417b9a83e7b6d183ee
Thomas Judd <tjudd@elitedaily.com>
2016-08-11 19:21:41 -0400

    adds function for generating test data

--- filedelivery.py
+++ filedelivery.py
@@ -2,13 +2,32 @@
 
 from flask import Flask, request, jsonify
 import math
+import random
+import string
 
 app = Flask(__name__)
 
 graph = {}
 
+def generate_nodes():
+  num_links = 30 
+  alphabet = list(string.ascii_lowercase)
+  for letter in alphabet:
+     graph[letter] = {
+       'name': letter,
+       'cost': 999999, #sufficiently large inital value gets overwritten in dijkstra
+       'visited': False,
+       'neighbor_keys': []
+     }
+  for i in range(num_links):
+    start = random.randint(0, len(alphabet)-1)
+    end = random.randint(0, len(alphabet)-1)
+    graph[alphabet[start]]['neighbor_keys'].append(graph[alphabet[end]]['name'])
+    graph[alphabet[end]]['neighbor_keys'].append(graph[alphabet[start]]['name'])
+
 @app.route('/')
 def index():
+  generate_nodes()
   return 'File Delivery REST API'
 
 @app.route('/host', methods=['POST', 'GET'])
@@ -59,8 +78,6 @@ def find_cheapest_path(host_a, host_b):
     curr_node = min_node
 
   return jsonify(visited)
-
-
 
 
 if __name__ == '__main__':
